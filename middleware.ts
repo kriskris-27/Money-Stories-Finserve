0f539ef8d0b27894fa08e4faa27fc6622f030f7a
kris <krishnakumars0101@gmail.com>
2026-02-19 17:41:24 +0530

    fix:build issue

--- middleware.ts
+++ middleware.ts
@@ -37,7 +37,7 @@ export default async function middleware(request: NextRequest) {
         return NextResponse.next();
     }
 
-    const ip = request.ip ?? "127.0.0.1";
+    const ip = (request as any).ip ?? request.headers.get("x-forwarded-for") ?? "127.0.0.1";
     const { success, limit, reset, remaining } = await ratelimit.limit(ip);
 
     if (!success) {
